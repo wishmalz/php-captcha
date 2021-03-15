fcf2f0e856345d8f6ef89d328020effb43f5d19f
Valeriy Bakalenko <valeriy.bakalenko@gmail.com>
2021-03-15 14:20:35 +0300

    Captcha generate and store in session

--- src/wishexxt/Captcha/captchaw.php
+++ src/wishexxt/Captcha/captchaw.php
@@ -19,3 +19,8 @@ $textColor = $config['textColor'];
 $noiseColor = $config['noiseColor'];
 $arrowColor = $config['arrowColor'];
 $bgColor = $config['bgColor'];
+
+for ($i = 0; $i < $totalCharacters; $i++) {
+    $captcha .= substr($possibleLetters, mt_rand(0, strlen($possibleLetters) - 1), 1);
+}
+$_SESSION['captcha'] = $captcha;
